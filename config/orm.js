3cdcfdfcbecdfd604faf2b07ed7c7cdf58d56782
Nicole Wilsey Starr <Nicole.WilseyStarr@gmail.com>
2019-07-09 21:44:21 -0500

    [config/orm.js] updated comments

--- config/orm.js
+++ config/orm.js
@@ -17,11 +17,11 @@ function objToSql(ob) {
     var value = ob[key];
     // check to skip hidden properties
     if (Object.hasOwnProperty.call(ob, key)) {
-      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      // if string with spaces, add quotations (Poutine on the Ritz Burger  => 'Poutine on the Ritz Burger')
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
-      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      // e.g. {name: 'Poutine on the Ritz Burger'} => ["name='Poutine on the Ritz Burger'"]
       // e.g. {sleepy: true} => ["sleepy=true"]
       arr.push(key + "=" + value);
     }
